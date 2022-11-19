9388ba3ce80f530a3ad122c45253bd50f07aa456
FalseR20 <kno3.2002@gmail.com>
2022-11-19 19:49:42 +0300

    save(lab4)

--- lab1234/data_module.py
+++ lab1234/data_module.py
@@ -1,6 +1,8 @@
+import json
 import pathlib
 import pickle
 from dataclasses import dataclass
+from pprint import pprint
 
 from PyQt6 import QtCore
 
@@ -18,6 +20,18 @@ class CalendarEventData:
 Data = dict[QtCore.QDate, list[CalendarEventData]]
 
 
+def data_to_json(data: Data) -> str:
+    return json.dumps(
+        {
+            date.toString(): [
+                {"description": calendar_event.description, "time": calendar_event.time.toString()}
+                for calendar_event in calendar_events
+            ]
+            for date, calendar_events in data.items()
+        }
+    )
+
+
 def save_data(data: Data) -> None:
     with open(_DATA_FILE, "wb") as file:
         pickle.dump(data, file)
@@ -27,4 +41,7 @@ def load_data() -> Data:
     if not _DATA_FILE.exists():
         return {}
     with open(_DATA_FILE, "rb") as file:
-        return pickle.load(file)
+        data = pickle.load(file)
+        pprint(data)
+        pprint(data_to_json(data))
+        return data
