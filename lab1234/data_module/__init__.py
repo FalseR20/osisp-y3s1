b1dd94b4f44b43737c414f7e4b4240d3a1aaf06d
FalseR20 <kno3.2002@gmail.com>
2022-11-30 21:44:36 +0300

    feat(lab1234): complete

--- lab1234/data_module/__init__.py
+++ lab1234/data_module/__init__.py
@@ -1,2 +1,2 @@
 from data_module.data_classes import CalendarEventData, Data
-from data_module.load_save import load_data, save_data
+from data_module.server_connector import load_data, save_data
---
+++ lab1234/data_module/server_connector.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+
+import requests
+
+from data_module.data_classes import Data
+from data_module.serializers import data_from_json, data_to_json
+
+logger = getLogger()
+
+URL = "http://127.0.0.1:5000/"
+
+
+def save_data(data: Data) -> None:
+    data_json = data_to_json(data)
+    logger.debug("Save data: %s", data_json)
+    requests.post(URL, data_json)
+
+
+def load_data() -> Data:
+    data_json = requests.get(URL).content.decode()
+    logger.debug("Load data: %s", data_json)
+    return data_from_json(data_json)
